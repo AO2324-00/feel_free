f485e9e2b159040dc9cca481d26f5035863cca23
AO2324-00 <ao.2324.00@gmail.com>
2021-11-28 13:22:08 +0900

    lend SQLの修正

--- controllers/Lend.py
+++ controllers/Lend.py
@@ -5,7 +5,7 @@ def get_lend(place_id):
     # 貸出状態の取得
     conn = sqlite3.connect(dbname)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM lend id = :place_id", { "place_id": place_id})
+    cur.execute("SELECT * FROM lend WHERE place_id = :place_id", { "place_id": place_id})
     data = []
     for row in cur:
         data.append({ "id":  row[0], "place_id": row[1], "begin_date": row[2], "end_date": row[3], "purpose":row[4]})
