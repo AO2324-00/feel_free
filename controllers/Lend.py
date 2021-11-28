aa40fc784310ae3f2b226ef06969c3a670234f97
AO2324-00 <ao.2324.00@gmail.com>
2021-11-28 12:46:44 +0900

    lend 目標2 完了

--- controllers/Lend.py
+++ controllers/Lend.py
@@ -1,7 +1,13 @@
 from flask import jsonify
 import sqlite3
-
 dbname = 'FeelFree.db'
-
 def get_lend(id):
-    return
+    # 貸出状態の取得
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM lend id = :id", { "id": id})
+    data = []
+    for row in cur:
+        data.append({ "id":  row[0], "place_id": row[1], "begin_date": row[2], "end_date": row[3], "purpose":row[4]})
+    conn.close()
+    return jsonify(data)
