2eea02baf455f9350dc948b992528132fa263b1e
AO2324-00 <ao.2324.00@gmail.com>
2021-11-28 12:46:02 +0900

    reserve目標3 完了

--- controllers/Reserve.py
+++ controllers/Reserve.py
@@ -1,38 +1,37 @@
 from flask import jsonify, request, make_response
 import sqlite3
-
 dbname = 'FeelFree.db'
-
 def reserve(request):
-
     body = request.json
     if body is None:
         return make_response('', 400)
-
     keydict = {"place_id" : None, "begin_date" : None, "end_date" : None, "purpose" : None}
-
     for key in body:
         if key not in keydict or body[key] is None:
             return make_response('', 400)
-
         else:
             keydict[key] = body[key]
-
+        
+    bdate = keydict["begin_date"]
+    edate = keydict["end_date"]
     conn = sqlite3.connect(dbname)
     cur = conn.cursor()
+    cur.execute("SELECT * FROM lend")
+    for row in cur:
+        bdate_check = row[2]
+        edate_check = row[3]
+        if (bdate_check <= bdate <= edate_check) or (bdate <= bdate_check <= edate):
+            return make_response('', 406)
     cur.execute("INSERT INTO lend (place_id, begin_date, end_date, purpose) VALUES (:place_id, :begin_date, :end_date, :purpose)",
     (
-    body['place_id'], \
-    body['begin_date'], \
-    body['end_date'], \
-    body['purpose'], \
+    keydict['place_id'], \
+    keydict['begin_date'], \
+    keydict['end_date'], \
+    keydict['purpose'], \
     ))
-
     conn.commit()
-
     # cur.execute("SELECT * FROM lend")
     # for row in cur:
     #     print(row)
-
     conn.close()    
     return make_response('', 200)
